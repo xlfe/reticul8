7e471231a763d4f1cb70f6e66e34acfd384b53c1
xlfe <felixb@gmail.com>
2018-09-20 15:24:16 +1000

    fixup makefile to build nanopb files in /python/

--- micro/extra/espidf_component.mk
+++ micro/extra/espidf_component.mk
@@ -8,15 +8,24 @@ COMPONENT_EXTRA_INCLUDES += $(NANOPB_DIR)
 COMPONENT_SRCDIRS += ../components/reticul8/nanopb
 PROTOC_OPTS += -I$(PGEN_DIR)
 
-$(PGEN_DIR)/%_pb2.py: $(PGEN_DIR)/%.proto
-	$(PROTOC) $(PROTOC_OPTS) --proto_path=$(PGEN_DIR) --python_out=$(PGEN_DIR) $<
+$(RETICUL8_DIR)/python/%_pb2.py: $(PGEN_DIR)/%.proto
+	$(PROTOC) $(PROTOC_OPTS) --proto_path=$(PGEN_DIR) --python_out=$(RETICUL8_DIR)/python/ $<
 
 # reticul8 - protoc
 $(RETICUL8_DIR)/micro/reticul8.pb.c: $(RETICUL8_DIR)/reticul8.proto
-	$(PROTOC) $(PROTOC_OPTS) --nanopb_out=$(RETICUL8_DIR)/micro/ --python_out=$(RETICUL8_DIR)/python/ --proto_path=$(RETICUL8_DIR) reticul8.proto
+	$(PROTOC) $(PROTOC_OPTS) --nanopb_out=$(RETICUL8_DIR)/micro/ --proto_path=$(RETICUL8_DIR) reticul8.proto
+
+$(RETICUL8_DIR)/python/reticul8_pb2.py: $(RETICUL8_DIR)/reticul8.proto
+	$(PROTOC) $(PROTOC_OPTS) --python_out=$(RETICUL8_DIR)/python/ --proto_path=$(RETICUL8_DIR) reticul8.proto
 
 COMPONENT_SRC += $(RETICUL8_DIR)/micro/reticul8.pb.c
-build: $(PGEN_DIR)/nanopb_pb2.py $(PGEN_DIR)/plugin_pb2.py $(RETICUL8_DIR)/micro/reticul8.pb.c
+build:  $(PGEN_DIR)/nanopb_pb2.py \
+        $(PGEN_DIR)/plugin_pb2.py \
+        $(RETICUL8_DIR)/micro/reticul8.pb.c \
+        $(RETICUL8_DIR)/python/nanopb_pb2.py \
+        $(RETICUL8_DIR)/python/plugin_pb2.py \
+        $(RETICUL8_DIR)/python/reticul8_pb2.py
+
 
 # reticul8
 COMPONENT_EXTRA_INCLUDES += $(RETICUL8_DIR)/micro
